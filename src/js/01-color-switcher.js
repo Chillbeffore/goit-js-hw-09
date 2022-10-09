e5e23e8431be351a887f7813d8e79a33f1468fd1
Богдан Опанасенко <bodia23042005@gmail.com>
2022-10-09 21:35:34 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timer = null;
+
+refs.stopBtn.setAttribute('disabled', true);
+
+refs.startBtn.addEventListener('click', () => {
+  timer = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.stopBtn.removeAttribute('disabled');
+  refs.startBtn.setAttribute('disabled', true);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
